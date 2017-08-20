07481d1874555654ef4d515e43f16e8c1795ffd4
drohanreddy <drohanreddy@gmail.com>
2017-08-20 22:08:30 +0530

    updated api

--- service/word.js
+++ service/word.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 var request = require("request-promise");
-var api_key= '7dc1cea6ac0b2ec90800600ab7d047ed0b8dcd9b9df217cb3';
+var api_key= 'API';
 var url=  'http://api.wordnik.com/v4/word.json/';
 var urls = 'http://api.wordnik.com/v4/words.json/';
 var propsObj= {includeDuplicates:false,useCanonical:true,skip:0,limit:10,api_key:'7dc1cea6ac0b2ec90800600ab7d047ed0b8dcd9b9df217cb3'};
